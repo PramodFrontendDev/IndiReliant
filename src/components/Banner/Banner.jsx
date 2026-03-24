ef66d6a4abe634a4222745de6a4d0bf91bd3fde3
PramodFrontendDev <pkushwah758@gmail.com>
2026-03-24 22:56:08 +0530

    Added slider

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,41 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import "./Banner.css";
-import bannerImage from "../../assets/images/banner.jpg";
+// import bannerImage from "../../assets/images/banner.jpg";
+
 
 const Banner = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
   return (
     <div className="banner-main">
-      <img src={bannerImage} alt="Banner" />
+      <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+    </Slider>
+      {/* <img src={bannerImage} alt="Banner" />
 
       <div className="banner-text">
         <h1>Welcome to Indi Reliant</h1>
         <p>Your reliable partner for all your needs.</p>
-      </div>
+      </div> */}
     </div>
   );
 };
